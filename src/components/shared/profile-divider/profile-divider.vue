0219fa1289e34b1a5554a1db5b8baf54098f8682
leonardossev <leonardossev@gmail.com>
2020-05-26 05:21:56 -0300

    feat(divider): implementa profile divider

---
+++ src/components/shared/profile-divider/profile-divider.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="profile-divider"></div>
+</template>
+
+<style lang="scss">
+.profile-divider {
+  background-color: #fff;
+  height: 0.1px;
+  width: 25%;
+  margin: 4% 0 1% 0;
+}
+
+</style>
